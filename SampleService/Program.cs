e17898d3f76f55fef96220246e0abca861eafdb3
Chris Patterson <chris@phatboyg.com>
2019-02-19 07:53:35 -0600

    Initial commit, working sample using .NET core Host

---
+++ SampleService/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SampleService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+                    services.Configure<AppConfig>(hostContext.Configuration.GetSection("AppConfig"));
+
+                    services.AddMassTransit(cfg => { cfg.AddBus(ConfigureBus); });
+
+                    services.AddSingleton<IHostedService, MassTransitConsoleHostedService>();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                });
+
+            await builder.RunConsoleAsync();
+        }
+
+        static IBusControl ConfigureBus(IServiceProvider provider)
+        {
+            var options = provider.GetRequiredService<IOptions<AppConfig>>().Value;
+
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(options.Host, options.VirtualHost, h =>
+                {
+                    h.Username(options.Username);
+                    h.Password(options.Password);
+                });
+            });
+        }
+    }
+}
